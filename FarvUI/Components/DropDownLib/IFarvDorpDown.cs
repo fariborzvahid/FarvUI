977b49c33c93b3f1fe4306c505e90d6d780647d2
Fariborz Vahid <fariborzvahid@gmail.com>
2024-01-15 18:47:25 +0330

    feat  : DropDown component base structure
    drop down component base structure

---
+++ FarvUI/Components/DropDownLib/IFarvDorpDown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarvUI.Components.DropDownLib;
+
+public interface IFarvDorpDownItem
+{
+    string GetName();
+    string GetId();
+    bool IsActive { get; set; }
+    bool IsVisible { get; set; }
+    bool IsChecked { get; set; }
+}
---
+++ FarvUI/README.md
@@ -0,0 +1,1 @@
+﻿# FarvUI Library 
